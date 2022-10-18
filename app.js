ca38c14456bb0c0d8c6448cd34933531cd164657
Ifty <iftikharimrul@iut-dhaka.edu>
2022-10-19 02:01:33 +0600

    error handling

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 
+//internal imports
+const {notFound, errorHandler} = require('./middlewares/errorHandler')
+
 dotenv.config()
 
 const conn = require('./serverDB')
@@ -14,25 +17,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+app.use(cookieParser(process.env.SECRET))
 app.set('view engine', 'ejs') // Setting EJS as template engine
 app.set('views', __dirname + '/views') // Setting the directory for the view files
 
-app.use('/', (req, res)=>{
-    res.send("home")
-})
+
+// app.use('/', (req, res)=>{
+//     res.send("home")
+// })
 
 
-
-
-
-
-
-
-
-
-
-
-
+//error Handlers
+app.use(notFound)
+app.use(errorHandler)
 
 
 app.listen(process.env.PORT, ()=>{
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,26 @@
+const createError = require('http-errors')
+//404
+
+const notFound = (req, res, next) => {
+    next(createError(404, "Page Not Found"))
+}
+
+const errorHandler = (err, req, res, next) => {
+
+    if(res.locals.html){
+        res.render('error', {
+            title:  "Error Page",
+            error: err,
+            status : err.status
+        })
+    }else{
+        res.json({
+            message: err.message,
+            status: err.status
+        })
+    }
+
+
+}
+
+module.exports = {notFound, errorHandler}
