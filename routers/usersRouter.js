ae04dbaf7ca67850c8cfecf6e06e208a30bcb207
Ifty <iftikharimrul@iut-dhaka.edu>
2023-02-11 13:19:24 +0600

    validate

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const {check} = require('express-validator')
 
 const {decorateHtmlresponse} = require('../middlewares/common/decorateHtmlResponse')
 const {getUsers} = require('../controllers/usersController')
+const avatarUoload = require('../middlewares/users/avatarUpload')
 
 router.get('/', decorateHtmlresponse('Users'),getUsers)
+
+router.post('/', avatarUoload, [
+        check('name').isLength({min: 1}).withMessage('Name is required'),
+        check('email').isEmail().withMessage('Email is required'),
+    ]);
+
 
 module.exports = router
