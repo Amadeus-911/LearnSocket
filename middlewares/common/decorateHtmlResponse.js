68606ebc0aa9f098400edefb2ad5c00bf33fd62e
Ifty <iftikharimrul@iut-dhaka.edu>
2023-02-13 14:33:41 +0600

    login

--- middlewares/common/decorateHtmlResponse.js
+++ middlewares/common/decorateHtmlResponse.js
@@ -2,6 +2,9 @@ const decorateHtmlresponse = (page_title) => {
     return (req, res, next) => {
         res.locals.html = true
         res.locals.title = page_title
+        res.locals.loggedInUser = {}
+        res.locals.data = {}
+        res.locals.errors = {}
         next()
     }
 }
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const {decorateHtmlresponse} = require('../middlewares/common/decorateHtmlResponse')
-
+const {doLoginValidator, doLoginValidatorHandler} = require('../middlewares/login/loginValidators')
 const {getLogin} = require('../controllers/loginController')
 
 router.get('/', decorateHtmlresponse('Login') ,getLogin)
 
-router.post('/', login)
+router.post('/', doLoginValidator, doLoginValidatorHandler, decorateHtmlresponse('login'), login)
 
 module.exports = router
